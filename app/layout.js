14a68e181d5a951ed4116a6b2ce6e5cfd118d0b5
Bilal-Jamil-121 <acrobatgammer@gmail.com>
2025-08-04 12:25:04 +0500

    Updated layout and background color

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body className={`bg-black ${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`bg-black antialiased`}>
        <Navbar/>
         {children}
       </body>
