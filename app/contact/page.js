ef69634b84540141c2b778685aedc4fd6295fa64
Bilal-Jamil-121 <acrobatgammer@gmail.com>
2025-08-05 13:14:37 +0500

    styles error resolved

--- app/contact/page.js
+++ app/contact/page.js
@@ -113,9 +113,9 @@ const Page = () => {
   };
 
   return (
-    <div className='mt-5 w-full  mx-auto md:px-100 bg-black min-h-screen'>
+    <div className='pt-5 w-full  mx-auto md:px-100 bg-black min-h-screen'>
       
-      <h1 className='flex justify-center font-bold text-2xl mb-4'>Quiz App</h1>
+      <h1 className='flex justify-center text-white font-bold text-2xl mb-4'>Quiz App</h1>
       <h1 className='flex text-red-500 justify-center font-bold text-2xl mb-4'>Time {counter} sec out of 100 sec</h1>
     { !start&&( <button onClick={()=>{
       setFinished(!finished);
